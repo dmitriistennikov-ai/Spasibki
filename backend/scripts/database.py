7c4779e86c0c1320fc1ecf10c6b32dce1314be08
dmitiy_mcp <d.stennikov@dpwp.ru>
2025-11-25 10:33:37 +0500

    Заменена БД (с lite на postgre)

--- backend/scripts/database.py
+++ backend/scripts/database.py
@@ -1,24 +1,3 @@
-# # Файл базы данных SQLite (создастся автоматически)
-# DATABASE_URL = "sqlite:///./database.db"
-#
-# # Создаём движок подключения
-# engine = create_engine(
-#     DATABASE_URL, connect_args={"check_same_thread": False}
-# )
-#
-# # Создаём класс для описания таблиц
-# Base = declarative_base()
-#
-# # Создаём фабрику для подключения к БД (сессии)
-# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-#
-# def get_db():
-#     db = SessionLocal()
-#     try:
-#         yield db
-#     finally:
-#         db.close()
-
 import os
 
 from dotenv import load_dotenv
