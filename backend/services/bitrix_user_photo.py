e95c0fa5062fb46322ee9cea134999a1675aa927
dmitriystennikov <dmitriistennikov@gmail.com>
2025-12-12 15:06:17 +0500

    Исправлен баг с аватаром пользователя

--- backend/services/bitrix_user_photo.py
+++ backend/services/bitrix_user_photo.py
@@ -12,11 +12,11 @@ async def get_user_photo(auth_id: str, refresh_id: str, domain: str, user_id: in
         "ID": user_id,
     })
 
-    if "result" not in user_data:
-        raise ValueError(user_data.get("error_description", "Ошибка получения пользователя"))
-
-    user_photo = user_data["result"][0]["PERSONAL_PHOTO"]
-    if not user_photo:
+    result = user_data.get("result") or []
+    if not result:
         return None
 
-    return user_photo
+    user = result[0] or {}
+    photo = user.get("PERSONAL_PHOTO")
+    
+    return photo or None
